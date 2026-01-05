6c8e959c3cff0099695d2955dad1b3a18b4df642
AmikMain <amikmain001@gmail.com>
2026-01-06 01:19:47 +0400

    Front Module Logic and switching logic

--- Assets/Scripts/ZombieDeathTrigger.cs
+++ Assets/Scripts/ZombieDeathTrigger.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 
-public class ZombieRagdollTrigger : MonoBehaviour
+public class ZombieDeathTrigger : MonoBehaviour, IDamageTrigger
 {
     [SerializeField] private float fatalSpeed = 10;
+
+    public void DealDamage(Collider other, int amount)
+    {
+        other.gameObject.GetComponent<Health>().TakeDamage(amount);
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Zombie") && GetComponentInParent<Rigidbody>().linearVelocity.magnitude >= fatalSpeed)
         {
-            other.gameObject.GetComponent<Health>().TakeDamage(100);
+            DealDamage(other, 100);
         }
     }
 }
+
+
+
